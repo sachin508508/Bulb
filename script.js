d21c201ba35d28a96ed59b0f86dc42c1d196b393
sachin508508 <sachin508508@gmail.com>
2023-09-05 15:57:27 +0530

    fijhsaijfl

---
+++ script.js
@@ -0,0 +1,10 @@
+function bon(){
+    document.getElementById('bgv').src="lighton.mp4";
+    document.getElementById('limage').src="lioff.png";
+    document.getElementById('bimage').src="boaton.jpg";
+}
+function boff(){
+    document.getElementById('bgv').src="lightoff.mp4"
+    document.getElementById('limage').src="lioff3.png";
+    document.getElementById('bimage').src="boatoff.jpg";
+}
